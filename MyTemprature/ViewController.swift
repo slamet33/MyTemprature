a93f72b4acdc5787d12d8ec67ff8599425bc29d2
Aditya Sanjaya <adityasanjaya24@gmail.com>
2019-11-12 16:19:39 +0700

    Finish calculation of Kelvin Converter

--- MyTemprature/ViewController.swift
+++ MyTemprature/ViewController.swift
@@ -15,6 +15,45 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    /*
+     Kelvin converter to Farenheit
+     To Do :
+     1. Get the parameter for the function
+     2. Wait the function to be done
+     3. Finish
+    */
+    func setKelvinToFarenheit(kelvin: Double) -> Double {
+        var farenheit : Double
+        let kelvinConst : Double = 273.15
+        let farenheitConst : Double = 9/5
+        let constanta : Double = 32.0
+        farenheit = (kelvin - kelvinConst) * farenheitConst + constanta
+        return farenheit
+    }
+    
+    /*
+     Kelvin converter to Celcius
+     To Do :
+     1. Get the parameter for the function
+     2. Wait the function to be done
+     3. Finish
+    */
+    func setKelvinToCelcius(kelvin: Double) -> Double {
+        var celcius : Double
+        let kelvinConst : Double = 273.15
+        celcius = kelvin + kelvinConst
+        return celcius
+    }
+    
+    /*
+     Kelvin converter to Kelvin
+     To Do :
+     1. Get the parameter for the function
+     2. Wait the function to be done
+     3. Finish
+    */
+    func setKelvinToKelvin(kelvin: Double) -> Double {
+        return kelvin
+    }
 }
 
