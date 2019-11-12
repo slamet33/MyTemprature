2ab67cbe54b0855787e91c45908f79b19d9072fc
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2019-11-12 16:17:23 +0700

    FahrenheitConverter

--- MyTemprature/ViewController.swift
+++ MyTemprature/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    final let kelvinBase:Double = 273.15
+    final let celciusBase:Double = 5/9
+    final let fahrenheitBase:Double = 32
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fahrenheit = fahrenheitToCelcius(fahrenheitValue: 30)
+        print(fahrenheit)
+    }
+    
+    func fahrenheitToCelcius(fahrenheitValue:Double) -> Double{
+        let celciusResult = (fahrenheitValue - fahrenheitBase) * celciusBase
+        return celciusResult
+    }
+    
+    func fahrenheitToKelvin(fahrenheitValue:Double) -> Double{
+        let kelvinResult = (fahrenheitValue - fahrenheitBase) * celciusBase + kelvinBase
+        return kelvinResult
     }
 
 
