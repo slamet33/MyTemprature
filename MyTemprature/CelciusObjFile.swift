eafe7ef8ae86f790bd0873c3ad44102640a05ba9
Ricky Effendi <rickricky.effendi@gmail.com>
2019-11-12 16:11:32 +0700

    add function celcius to fahrenheit and celcius to kelvin

---
+++ MyTemprature/CelciusObjFile.swift
@@ -0,0 +1,30 @@
+//
+//  CelciusObjFile.swift
+//  MyTemprature
+//
+//  Created by Ricky Effendi on 12/11/19.
+//  Copyright © 2019 Slamet Riyadi. All rights reserved.
+//
+
+import UIKit
+
+class Celcius {
+    var temperature: Double
+    
+    // initialize temperature for celcius
+    init(celTemp: Double) {
+        self.temperature = celTemp
+    }
+    
+    // convert celcius to farenheit
+    func celToFar(celTemp: Double) -> Double {
+        let farTemp = celTemp * 1.8 + 32
+        return farTemp
+    }
+    
+    // convert celcius to kelvin
+    func celToKel(celTemp: Double) -> Double {
+        let kelTemp = celTemp + 273.15
+        return kelTemp
+    }
+}
