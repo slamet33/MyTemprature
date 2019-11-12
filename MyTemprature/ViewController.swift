b822fdc2436c06a6308961d42653b198196e90a4
Slamet <slametngeblog@gmail.com>
2019-11-12 15:57:22 +0700

    Initial Commit

---
+++ MyTemprature/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTemprature
+//
+//  Created by Qiarra on 12/11/19.
+//  Copyright © 2019 Slamet Riyadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
